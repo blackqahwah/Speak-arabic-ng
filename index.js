49bf87628ed181cfff2ef5fdeed74cf5e5c31413
Rahmatullaah Boluwatife Ibraheem <67290908+blackqahwah@users.noreply.github.com>
2023-02-05 06:04:03 +0000

    bootstrap and jquery installed. kitchen directory added

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require('express');
-
+const path = require("path");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello Express app!')
+// const bootstrap = require('bootstrap');
+
+
+app.use(express.static("kitchen"));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "kitchen", "index.html"));
+// });
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "kitchen", "headers.css"));
+// });
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
 
-app.listen(3000, () => {
-  console.log('server started');
-});
+
+
+
+
+
+
